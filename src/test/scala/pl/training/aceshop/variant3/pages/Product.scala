1593e7ac58732689087e87d44dddbe451dbda479
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-05-15 13:13:47 +0200

    1

---
+++ src/test/scala/pl/training/aceshop/variant3/pages/Product.scala
@@ -0,0 +1,19 @@
+package pl.training.aceshop.variant3.pages
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
+object Product {
+
+  def details(productName: String): ChainBuilder = exec(
+    http(s"Load Product Detail - ${productName}")
+      .get(s"/product/${productName}")
+  )
+
+  def addToCart(productId: Int): ChainBuilder = exec(
+    http(s"Add to Cart: ${productId}")
+      .get(s"/cart/add/${productId}")
+  )
+
+}
